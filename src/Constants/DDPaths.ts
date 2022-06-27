5528efe1cfa5870ccc9d54a096151851a3c7025c
David Kanders <kanders.davids@gmail.com>
2022-06-27 22:58:08 +0300

    League 12.11 => 12.12

--- src/Constants/DDPaths.ts
+++ src/Constants/DDPaths.ts
@@ -1,13 +1,32 @@
-const VERSION = '12.12.1'
-const BASE = `http://ddragon.leagueoflegends.com/cdn/${VERSION}`
+const version = '12.12.1'
+const base = `http://ddragon.leagueoflegends.com/cdn/${version}`
 
 /**
  * DataDragon CDN Paths.
- * All paths end with '/'.
  */
 export const DDPaths = {
-  CHAMPION_ICON_BASE: `${BASE}/img/champion/`,
-  SPELL_ICON_BASE: `${BASE}/img/spell/`,
-  PROFILE_ICON_BASE: `${BASE}/img/profileicon/`,
-  ITEM_ICON_BASE: `${BASE}/img/item/`,
+  /**
+   * Append with `/MonkeyKing.png` (`/{championId}.png`).
+   */
+  CHAMPION_ICON_BASE: `${base}/img/champion`,
+  /**
+   * Append with `/MonkeyKing_0.png` (`/{championId}_{skinNum}.png`).
+   */
+  CHAMPION_LOADING_BASE: `${base}/img/champion/loading`,
+  /**
+   * Append with `/MonkeyKing_0.png` (`/{championId}_{skinNum}.png`).
+   */
+  CHAMPION_SPLASH_BASE: `${base}/img/champion/splash`,
+  /**
+   * Append with `/FlashFrost.png` (`/{abilityKey}.png`).
+   */
+  SPELL_ICON_BASE: `${base}/img/spell`,
+  /**
+   * Append with `/685.png` (`/{iconId}.png`).
+   */
+  PROFILE_ICON_BASE: `${base}/img/profileicon`,
+  /**
+   * Append with `/1001.png` (`/{itemId}.png`).
+   */
+  ITEM_ICON_BASE: `${base}/img/item`,
 } as const
--- src/Constants/SpellNames.ts
+++ src/Constants/SpellNames.ts
@@ -1,18 +1,1 @@
-export const SpellNames = {
-  '1': 'Cleanse',
-  '3': 'Exhaust',
-  '4': 'Flash',
-  '6': 'Ghost',
-  '7': 'Heal',
-  '11': 'Smite',
-  '12': 'Teleport',
-  '13': 'Clarity',
-  '14': 'Ignite',
-  '21': 'Barrier',
-  '30': 'To the King!',
-  '31': 'Poro Toss',
-  '32': 'Mark',
-  '39': 'Mark',
-  '54': 'Placeholder',
-  '55': 'Placeholder and Attack-Smite',
-} as const
+export const SpellNames = {"1":"Cleanse","3":"Exhaust","4":"Flash","6":"Ghost","7":"Heal","11":"Smite","12":"Teleport","13":"Clarity","14":"Ignite","21":"Barrier","30":"To the King!","31":"Poro Toss","32":"Mark","39":"Mark","54":"Placeholder","55":"Placeholder and Attack-Smite"} as const
